0dec913851321dc718eec1861591c1b836e3e0ae
FiNaLiZee <sd103e@mail.ru>
2022-01-23 20:03:49 +0300

    Create dropdown ico

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -4,9 +4,8 @@
 
     <div class="dropdown">
       <button class="btn btn-light dropdown-toggle" id="dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-        Dropdown link
+        Create new
       </button>
-
       <ul class="dropdown-menu" aria-labelledby="dropdown">
         <li class="dropdown-item"><a href="#">Password</a></li>
         <li class="dropdown-item"><a href="#">Note</a></li>
@@ -17,6 +16,8 @@
     <ul>
       <li><a class="text-white" href="#">passwords</a></li>
       <li><a class="text-white" href="#">notes</a></li>
+      <li><a class="text-white" href="#">audit</a></li>
+      <li><a class="text-white" href="#">trash bin</a></li>
     </ul>
   </aside>
 </template>
@@ -24,5 +25,11 @@
 <style scoped lang="scss">
 .text-white:hover {
   color: #C0C4CC !important;
+}
+
+.dropdown-toggle::after {
+  content: url("assets/plus-lg.svg");
+  vertical-align: -.15em;
+  border: none;
 }
 </style>
