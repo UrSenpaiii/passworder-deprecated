ec0d1c166bd213847c21a21c15cd4299fcec9432
FiNaLiZee <sd103e@mail.ru>
2022-02-14 01:21:44 +0300

    create item component for list layout

---
+++ components/vault_layouts/ItemLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <li class="d-flex bg-white px-3 py-1 border">
+    <span v-if="num" class="align-self-center text-muted">{{ `${num}.` }}</span>
+    <i class="bi bi-file-earmark h3 align-self-center mx-1"></i>
+    <div>
+      <div class="h5 my-0">{{ title }}</div>
+      <div class="text-muted h6">{{ login }}</div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {num: Number, title: String, login: String},
+  data: () => ({})
+}
+</script>
+
+<style scoped>
+li:hover {
+  background-color: #F7F7F7 !important;
+  cursor: pointer;
+}
+</style>
--- components/vault_layouts/ListLayout.vue
+++ components/vault_layouts/ListLayout.vue
@@ -1,5 +1,7 @@
 <template>
-
+  <ul class="p-0">
+    <v-item-layout v-for="(record, i) in records" :key="i" :num="i+1" :title="record.title" :login="record.login"/>
+  </ul>
 </template>
 
 <script>
