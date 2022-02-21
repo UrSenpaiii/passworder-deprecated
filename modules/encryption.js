7f52156720c42bf86fe8cc9dec9f81f5e544cc7f
FiNaLiZee <sd103e@mail.ru>
2022-02-21 03:39:38 +0300

    Remove scrypt-js coz decided to use sha256 with build-in createHash method

--- modules/encryption.js
+++ modules/encryption.js
@@ -1,42 +1,24 @@
-const crypto = require("crypto")
-const {Buffer} = require("buffer")
-const scrypt = require("scrypt-js")
+const {randomBytes, createHash, createCipheriv, createDecipheriv} = require("crypto")
 
 const ENCODING_FORMAT = "hex"
 const DECODING_FORMAT = "utf8"
 const ALGORITHM = "aes256"
 
-let keyToHex = (str) => {
-  // N - The CPU/memory cost; increases the overall difficulty
-  // r - The block size; increases the dependency on memory latency and bandwidth
-  // p - The parallelization cost; increases the dependency on multiprocessing
-  const salt = "f8VQcI86MUgLzQCYsKRV"
-  const N = 1024, r = 8, p = 1
-  const dkLen = 16
-
-  let buffedStr = Buffer.from(str, DECODING_FORMAT)
-  let buffedSalt = Buffer.from(salt, DECODING_FORMAT)
-
-  let uint8ArrKey = scrypt.syncScrypt(buffedStr, buffedSalt, N, r, p, dkLen)
-
-  return Buffer.from(uint8ArrKey).toString(ENCODING_FORMAT)
-}
-
 export let encrypt = (str, key) => {
-  const iv = crypto.randomBytes(8).toString(ENCODING_FORMAT)
-  const hexedKey = keyToHex(key) // todo export
-  const cipher = crypto.createCipheriv(ALGORITHM, hexedKey, iv)
+  const iv = randomBytes(8).toString(ENCODING_FORMAT)
+  const hashedKey = createHash("sha256").update(key).digest()
+  const cipher = createCipheriv(ALGORITHM, hashedKey, iv)
 
   let encrypted = cipher.update(str, DECODING_FORMAT, ENCODING_FORMAT)
   encrypted += cipher.final(ENCODING_FORMAT)
 
-  return `${encrypted}:${iv}` // todo separator
+  return `${encrypted}:${iv}`
 }
 
 export let decrypt = (str, key) => {
   const [encryptedStr, iv] = str.split(":")
-  const hexedKey = keyToHex(key)
-  const decipher = crypto.createDecipheriv(ALGORITHM, hexedKey, iv)
+  const hashedKey = createHash("sha256").update(key).digest()
+  const decipher = createDecipheriv(ALGORITHM, hashedKey, iv)
 
   let decrypted = decipher.update(encryptedStr, ENCODING_FORMAT, DECODING_FORMAT)
   decrypted += decipher.final(DECODING_FORMAT)
