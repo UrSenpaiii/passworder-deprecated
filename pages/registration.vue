04da9b5afa94bc575905274e1480cf529c2fae35
FiNaLiZee <sd103e@mail.ru>
2022-02-26 17:53:51 +0300

    Update email regex (1st domen was only 1ch and total length was passing cyrillic)

--- pages/registration.vue
+++ pages/registration.vue
@@ -14,8 +14,7 @@
         <div class="mb-3">
           <label class="form-label" for="email">{{ $t("email") }}</label>
           <input id="email" class="form-control" maxlength="50" minlength="6" name="email" required type="email"
-                 pattern="(?:[a-z0-9!#$%&*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&*+/=?^_`{|}~-]+)*|'(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*')@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
-                 @input="validateEmailField">
+                 pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" @input="validateEmailField">
           <div class="invalid-feedback">
             {{ errors.email[errorIndexes[1]] }}
           </div>
