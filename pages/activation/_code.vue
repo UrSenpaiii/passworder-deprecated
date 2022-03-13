5bf6102ebc7ae0ee5ede6bb6203b9bce865b2ae3
FiNaLiZee <sd103e@mail.ru>
2022-03-13 20:01:01 +0300

    add activation code page

---
+++ pages/activation/_code.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  auth: false,
+  methods: {
+    activate() {
+      this.$axios.get(`/activation/${this.route.props.code}`, this.route.props.code)
+        .then(res => console.log(res))
+        .catch(e => console.log(e))
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
