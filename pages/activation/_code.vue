d2e014d07dd41378a41e8fbfbff1404051fd33f3
FiNaLiZee <sd103e@mail.ru>
2022-03-16 00:31:38 +0300

    Commit from Fedora35
    Lines' ending changed

--- pages/activation/_code.vue
+++ pages/activation/_code.vue
@@ -5,12 +5,10 @@
 <script>
 export default {
   auth: false,
-  methods: {
-    activate() {
-      this.$axios.get(`/activation/${this.route.props.code}`, this.route.props.code)
-        .then(res => console.log(res))
-        .catch(e => console.log(e))
-    }
+  mounted() {
+    this.$axios.get(`/activation/${this.$route.params.code}`)
+      .then(res => console.log(res))
+      .catch(e => console.log(e))
   }
 }
 </script>
