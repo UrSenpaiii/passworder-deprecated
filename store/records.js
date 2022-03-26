306294ca91a6b557ff74a528042a52447ddeea93
FiNaLiZee <sd103e@mail.ru>
2022-03-26 19:56:19 +0300

    Remove decrypt | move sendData | UPD records tree mock

--- store/records.js
+++ store/records.js
@@ -1,67 +1,69 @@
-import {decrypt} from "@/modules/encryption"
-
 export const state = () => ({
-  encryptedData: null,
-  records: [],
-  recordss: [
-    {
-      title: "",
-      username: "",
-      password: "",
-      notes: "",
-      website: "",
-      customFields: {
-        field1: "",
-        field2: ""
-      },
-      tags: [],
-      created: "",
-      updated: "",
-      expired: ""
-    }, {
-      title: "",
-      username: "",
-      password: "",
-      notes: "",
-      website: "",
-      customFields: {
-        field1: "",
-        field2: ""
-      },
-      tags: [],
-      created: "",
-      updated: "",
-      expired: "",
-      children: [
-        {
-          title: "",
-          username: "",
-          password: "",
-          notes: "",
-          website: "",
-          customFields: {
-            field1: "",
-            field2: ""
-          },
-          tags: [],
-          created: "",
-          updated: "",
-          expired: ""
-        }
-      ]
-    }
-  ],
+  encryptedData: [],
+  records: {
+    Database1: {
+      "Default Group": [{
+        title: "Google",
+        username: "vasy@gmail.com",
+        password: "12345678",
+        notes: "hello",
+        website: "https://google.com",
 
+        "Телефон": "+1463664324",
+
+        tags: ["service", "email"],
+        created: Date.now(),
+        updated: Date.now(),
+        expired: Date.now()
+      }, {
+        title: "Facebook",
+        username: "89462742625",
+        password: "27Tdi88qk4n15fTV",
+        notes: "",
+        website: "https://facebook.com",
+
+        Email: "affda@mail.ya",
+        "Recovery codes": "spidfgbjopadfijdsajvfo  asdjfl$adsjfadosp asdfjas4%lodjf",
+
+        tags: ["Social network"],
+        created: Date.now(),
+        updated: null,
+        expired: null
+      }, {
+        "VKontakte": [{
+          title: "VK",
+          username: "89462742625",
+          password: "vbTUBE41iJ1T58si",
+          notes: "Primary",
+          website: "https://vk.com",
+
+          Email: "mymail@bk.com",
+
+          tags: ["Social network"],
+          created: Date.now(),
+          updated: null,
+          expired: null,
+        }, {
+          title: "VK",
+          username: "89615071175",
+          password: "vbTUBE41iJ1T58si",
+          notes: "Secondary",
+          website: "https://vk.com",
+          tags: ["Social network"],
+          created: Date.now(),
+          updated: null,
+          expired: null
+        }]
+      }]
+    },
+  },
   validKey: false
 })
 
 export const mutations = {
-  decryptRecords(state, data) {
-    state.invalidKey = false
-    let records = decrypt(data.encryptedData, data.masterKey)
-    if (records == null) state.invalidKey = true
-    else
-      for (let record in records) state.records.push(record)
+  // Setters
+  setEncryptedData(state, encryptedData) {
+    state.encryptedData = encryptedData
   }
 }
 
@@ -75,9 +77,6 @@ export const actions = {
 
   },
 
-  // Setters
-  setEncryptedData(state, data) {
-    state.encryptedData = data + 1
   }
 }
 
