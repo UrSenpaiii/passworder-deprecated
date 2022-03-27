e4cbf4e9dfd1d86a420b4d2dddfd0734774524bb
FiNaLiZee <sd103e@mail.ru>
2022-03-27 16:31:20 +0300

    Improve titles array and add createDB card

--- pages/vaults/index.vue
+++ pages/vaults/index.vue
@@ -4,10 +4,20 @@
       <v-sidebar/>
       <div class="col-10">
         <div class="row">
-          <div class="card col-1 m-3" v-for="entry in $store.state.records.encryptedData" :key="entry.title">
+          <nuxt-link v-if="!titles.length" :to="localePath('/vaults/create')"
+                     class="card col-2 w-auto text-center m-3 p-0">
+            <div id="newVault" class="card-body">
+              <h5 class="card-title">Create new vault</h5>
+              <i class="bi bi-plus h1"/>
+            </div>
+          </nuxt-link>
+
+          <div class="card col-1 m-3 text-center" v-for="title in titles" :key="title">
             <div class="card-body">
-              <h5 class="card-title">{{ entry.title }}</h5>
-              <a href="#" class="btn btn-primary">{{ $t("btn.enter") }}</a>
+              <h5 class="card-title">{{ title }}</h5>
+              <nuxt-link :to="localePath(`/vaults/${title}`)" class="btn btn-primary">
+                {{ $t("btn.enter") }}
+              </nuxt-link>
             </div>
           </div>
         </div>
@@ -29,10 +39,15 @@ export default {
       ]
     }
   },
-  async fetch({store}){
+  data: () => ({
+    titles: []
+  }),
+  async fetch({store}) {
     await store.dispatch("records/requestVaultsData")
   },
-
+  mounted() {
+    this.titles = this.$store.state.records.encryptedData.map(e => e.title)
+  }
   // methods: {
   //   ...mapActions({
   //     requestVaultData: "records/requestVaultData"
@@ -43,3 +58,12 @@ export default {
   // }
 }
 </script>
+
+<style scoped lang="scss">
+#newVault {
+  transition: all .3s;
+  &:hover {
+    box-shadow: 0 .5rem 1rem rgba(0, 0, 0, .15) !important;
+  }
+}
+</style>
