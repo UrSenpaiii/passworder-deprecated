97e93f04151b59e64140c36c27860d9cb7f0ba31
FiNaLiZee <sd103e@mail.ru>
2022-03-31 00:05:19 +0300

    add dropdown for logout on header

--- components/Header.vue
+++ components/Header.vue
@@ -6,9 +6,18 @@
         <nuxt-link :to="localePath('/vaults')" class="navbar-brand me-auto ms-5">{{ $t("vault") }}</nuxt-link>
 
 
-        <div class="d-flex" v-if="$auth.loggedIn">
-          <i class="h2 text-white bi-person-circle"></i>
-          <nuxt-link :to="localePath('/profile')" class="nav-link text-white">{{ $auth.user.username }}</nuxt-link>
+        <div class="d-flex me-4" v-if="$auth.loggedIn">
+          <div class="dropdown">
+            <div class="d-flex dropdown-toggle" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="h2 text-white bi-person-circle"/>
+              <a class="nav-link text-white">
+                {{ $auth.user.username }}
+              </a>
+            </div>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+              <a class="dropdown-item" @click="logout">{{ $t("logout") }}</a>
+            </div>
+          </div>
         </div>
         <div v-else>
           <nuxt-link :to="localePath('/registration')" class="btn btn-light">{{ $t("signUp") }}</nuxt-link>
@@ -23,6 +32,11 @@
 export default {
   data: () => ({
     sitename: process.env.name
-  })
+  }),
+  methods: {
+    async logout() {
+      await this.$auth.logout()
+    }
+  }
 }
 </script>
