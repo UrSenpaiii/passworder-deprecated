c64975659d3126d855c6c605c0dde2b9a465b700
FiNaLiZee <sd103e@mail.ru>
2022-03-31 00:47:29 +0300

    Add template for error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>{{ $t("error") }}</h1>
+    <div class="container my-5 text-center">
+      <div class="notfound-404">
+        <h1>404</h1>
+      </div>
+      <h2>Oops! This Page Could Not Be Found</h2>
+      <p>Sorry but the page you are looking for does not exist, have been removed. name changed or is temporarily unavailable</p>
+      <nuxt-link class="btn btn-secondary" :to="localePath('/')">Go To Homepage</nuxt-link>
+    </div>
 </template>
