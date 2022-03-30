fe31338be80c6b739e15658733b7cc2e6a17d595
FiNaLiZee <sd103e@mail.ru>
2022-03-31 00:03:41 +0300

    Button for lock vault and create

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -3,23 +3,24 @@
     <div class="align-items-baseline d-flex my-3">
       <h2 class="text-white">{{ $t("myVault") }}</h2> <i class="bi bi-list mx-3 text-white"/>
     </div>
-
-    <div class="dropdown my-3">
-      <button class="btn btn-light dropdown-toggle" id="dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-        {{ $t("create") }}
-      </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdown">
-        <li class="dropdown-item">
-          <a class="d-block">{{ $t("record") }}</a>
-        </li>
-        <li class="dropdown-item">
-          <a class="d-block">{{ $t("folder") }}</a>
-        </li>
-        <li class="dropdown-item">
-          <a class="d-block">{{ $t("vault") }}</a>
-        </li>
-      </ul>
+    <div v-if="$route.path !== '/vaults'">
+      <div class="dropdown my-3">
+        <button class="btn btn-light dropdown-toggle" id="dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+          {{ $t("create") }}
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdown">
+          <li class="dropdown-item"><a class="d-block">{{ $t("record") }}</a></li>
+          <li class="dropdown-item"><a class="d-block">{{ $t("folder") }}</a></li>
+        </ul>
+      </div>
+      <div class="my-3">
+        <nuxt-link :to="localePath('/vaults')" class="btn btn-outline-light bi bi-shield-lock"> {{ $t("lock") }}
+        </nuxt-link>
+      </div>
     </div>
+    <nuxt-link v-else :to="localePath('/vaults/create')" class="text-white h4">
+      {{ $t("createVault") }} <i class="bi bi-plus-circle"/>
+    </nuxt-link>
   </aside>
 </template>
 
